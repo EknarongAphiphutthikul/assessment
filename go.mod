1bb49ed3b2ea6c0e508d6e92a52fdbead1ad99fc
EknarongAphiphutthikul <akenarongake@hotmail.com>
2023-01-04 08:24:09 +0700

    add logrus

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/EknarongAphiphutthikul/assessment
 
 go 1.19
 
-require github.com/lib/pq v1.10.7
+require (
+	github.com/lib/pq v1.10.7
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- server.go
+++ server.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"database/sql"
-	"log"
+	"os"
 
 	"github.com/EknarongAphiphutthikul/assessment/pkg/common"
 	"github.com/EknarongAphiphutthikul/assessment/pkg/config"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	log := initialLog()
+
 	config := config.NewConfig()
-	db := initialPostgres(config)
+	log.Info("Load Config success.")
+
+	db := initialPostgres(config, log)
 	defer db.Close()
-	log.Print("Database store initial success.")
+	log.Info("Database store initial success.")
 }
 
-func initialPostgres(config config.Config) *sql.DB {
+func initialLog() *logrus.Logger {
+	return &logrus.Logger{
+		Out:          os.Stderr,
+		Formatter:    new(logrus.JSONFormatter),
+		Hooks:        make(logrus.LevelHooks),
+		Level:        logrus.InfoLevel,
+		ExitFunc:     os.Exit,
+		ReportCaller: false,
+	}
+}
+
+func initialPostgres(config config.Config, log *logrus.Logger) *sql.DB {
 	createTable := `
 		CREATE TABLE IF NOT EXISTS expenses (id SERIAL PRIMARY KEY, title TEXT,	amount FLOAT,	note TEXT,	tags TEXT[]	);
 	`
