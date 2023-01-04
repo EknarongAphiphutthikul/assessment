b0661ae4d73f190c74b1582ea5118065b0d32544
EknarongAphiphutthikul <akenarongake@hotmail.com>
2023-01-04 11:32:12 +0700

    add echo framework

--- go.mod
+++ go.mod
@@ -3,11 +3,21 @@ module github.com/EknarongAphiphutthikul/assessment
 go 1.19
 
 require (
+	github.com/labstack/echo/v4 v4.10.0
 	github.com/lib/pq v1.10.7
 	github.com/sirupsen/logrus v1.9.0
 )
 
 require (
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.2.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/time v0.2.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/EknarongAphiphutthikul/assessment/pkg/common"
 	"github.com/EknarongAphiphutthikul/assessment/pkg/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/sirupsen/logrus"
 )
 
@@ -18,6 +25,8 @@ func main() {
 	db := initialPostgres(config, log)
 	defer db.Close()
 	log.Info("Database store initial success.")
+
+	gracefulShutdown(startServer(config, log), log)
 }
 
 func initialLog() *logrus.Logger {
@@ -46,4 +55,50 @@ func initialPostgres(config config.Config, log *logrus.Logger) *sql.DB {
 	}
 
 	return db
+}
+
+func startServer(config config.Config, log *logrus.Logger) *http.Server {
+	e := echo.New()
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:      true,
+		LogStatus:   true,
+		LogRemoteIP: true,
+		LogValuesFunc: func(c echo.Context, values middleware.RequestLoggerValues) error {
+			log.WithFields(logrus.Fields{
+				"URI":    values.URI,
+				"status": values.Status,
+			}).Info("request")
+
+			return nil
+		},
+	}))
+	e.Use(middleware.Recover())
+
+	srv := &http.Server{
+		Addr:    ":" + config.Port(),
+		Handler: e,
+	}
+
+	log.Infof("App started. PORT=%v", config.Port())
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("shutting down the server %v", err)
+		}
+	}()
+
+	return srv
+}
+
+func gracefulShutdown(srv *http.Server, log *logrus.Logger) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	<-signals
+
+	log.Info("App is shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Error shutting down: %v", err)
+	}
+	log.Info("Bye")
 }
